de6e84e3eb65601f593475639299956ad5f05396
hitalo07 <hitalocunhadesousa@gmail.com>
2019-09-22 13:10:37 -0300

    feat: Add Redux and create SignIn

---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,34 @@
+import { call, put, all, takeLatest } from 'redux-saga/effects'
+import { toast } from 'react-toastify'
+
+import api from '~/services/api'
+import history from '~/services/history'
+
+import { signInSuccess, signFailure } from './actions'
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload
+
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password
+    })
+
+    const { token, user } = response.data
+
+    if (!user.provider) {
+      toast.error('O usuario nao e prestador de servico.')
+      return
+    }
+
+    yield put(signInSuccess(token, user))
+
+    history.push('/dashboard')
+  } catch (err) {
+    toast.error('Falha na autenticacao, verifique seus dados')
+    yield put(signFailure())
+  }
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)])
